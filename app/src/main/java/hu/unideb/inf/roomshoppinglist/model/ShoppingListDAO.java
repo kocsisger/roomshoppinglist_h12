2612e29d4151465002b6e459b588b7b4bc082130
Kocsis Gergely <kocsisger@gmail.com>
2026-04-13 13:21:18 +0200

    Test 1

--- app/src/main/java/hu/unideb/inf/roomshoppinglist/model/ShoppingListDAO.java
+++ app/src/main/java/hu/unideb/inf/roomshoppinglist/model/ShoppingListDAO.java
@@ -1,8 +1,16 @@
 package hu.unideb.inf.roomshoppinglist.model;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface ShoppingListDAO {
-    void insertListItem(ShoppingListI);
+    @Insert
+    public void insertListItem(ShoppingListItem sli);
+
+    @Query("SELECT * FROM ShoppingList")
+    public List<ShoppingListItem> getAllItems();
 }
